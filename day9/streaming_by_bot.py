7e31e9f7e8754fbb64886ebcb5b1ee2c71a599af
Ravi Tiwari <raviwrites07@gmail.com>
2026-08-06 01:32:59 +0530

    Add day9 streaming chatbot project

---
+++ day9/streaming_by_bot.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+load_dotenv()
+
+my_api_key=os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("api key kahan hai bhai?")
+client = Groq(api_key=my_api_key)
+model="llama-3.3-70b-versatile"
+prompt="how internet works?"
+message ={
+    "role": "user",
+    "content": prompt
+
+}
+messages=[message]
+stream=client.chat.completions.create(model=model, messages=messages, stream=True)
+for chunk in stream:
+    content=chunk.choices[0].delta.content
+    if content:
+        print(content, end="", flush=True)
